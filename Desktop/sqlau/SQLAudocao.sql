29a48dbb715d78aec6c7077028bee92d353dafd8
PaulaCami <164426057+PaulaCami@users.noreply.github.com>
2024-04-26 20:44:32 -0300

    Delete Desktop/sqlau directory

--- Desktop/sqlau/SQLAudocao.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Audocao1 (
-id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100),
-    email VARCHAR(100),
-    telefone VARCHAR(20),
-    endereco VARCHAR(255),
-    data_nascimento DATE,
-    sexo ENUM('M', 'F', 'Outro')
-);
